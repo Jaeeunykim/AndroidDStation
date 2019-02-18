f6bcc53c5da3939229fa6c0301c5b6312e5c6def
Jaeeunykim <jaeeun.kje@gmail.com>
2019-02-19 00:27:48 +0900

    change data

---
+++ DStation2/app/src/main/java/io/subak/dstation/MApplication.java
@@ -0,0 +1,15 @@
+package io.subak.dstation;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.secneo.sdk.Helper;
+
+public class MApplication  extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        Helper.install(MApplication.this);
+    }
+}
