c2dafdc95980b5be018ec76b26083c7f6fcaad20
Jaeeunykim <jaeeun.kje@gmail.com>
2019-03-06 02:29:11 +0900

    레이아웃 추가

--- NaverTestMap/app/src/main/java/io/subak/navertestMap/MainActivity.java
+++ NaverTestMap/app/src/main/java/io/subak/navertestMap/MainActivity.java
@@ -3,29 +3,11 @@ package io.subak.navertestMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.nhn.android.maps.NMapActivity;
-import com.nhn.android.maps.NMapView;
-
-public class MainActivity extends NMapActivity {
-
-    private NMapView mMapView;
-    private final String CLIENT_ID="juqony0mqu";
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mMapView = new NMapView(this);
-        setContentView(mMapView);
-        mMapView.setClientId(CLIENT_ID);
-
-        mMapView.setClickable(true);
-        mMapView.setEnabled(true);
-        mMapView.setFocusable(true);
-        mMapView.setFocusableInTouchMode(true);
-        mMapView.requestFocus();
-
-
-
+        setContentView(R.layout.activity_main);
     }
 }
