ca61987ed1adc4523a2467e8200d0143303b5159
Jaeeunykim <jaeeun.kje@gmail.com>
2019-03-06 01:44:30 +0900

    네이버 bintray설정

--- NaverTestMap/README.md
+++ NaverTestMap/README.md
@@ -4,4 +4,7 @@
 ##2. gradle(app)
 ##3. Manifase permission 설정 
 ##4. NMapActivity에서 상속 
-##4. map Activity의 onCreate에서 네이버 맵 초기화 
+##5. map Activity의 onCreate에서 네이버 맵 초기화 
+
+##6. 의존성 추가 네이버 bintray 추가
+
--- NaverTestMap/app/src/main/java/io/subak/navertestMap/MainActivity.java
+++ NaverTestMap/app/src/main/java/io/subak/navertestMap/MainActivity.java
@@ -4,12 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapView;
 
 public class MainActivity extends NMapActivity {
+
+    private NMapView mMapView;
+    private final String CLIENT_ID="juqony0mqu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mMapView = new NMapView(this);
+        setContentView(mMapView);
+        mMapView.setClientId(CLIENT_ID);
+
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.requestFocus();
+
+
+
     }
 }
