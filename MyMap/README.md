e29ea4e4c47db616fd050e47c2bb17b0bf82ac83
Jaeeunykim <jaeeun.kje@gmail.com>
2019-03-06 14:57:39 +0900

    mutiDex오류 수정

---
+++ MyMap/README.md
@@ -0,0 +1,7 @@
+#to do 
+#1. gralde 맵라이브러리 버전 변경 
+#2. gralde project maven추가 
+#3. mapActivity 상속삭제 
+#4. 네이버 맵키 매니패스트에 등록
+#5. multidex 설정 추가 
+
--- MyMap/app/src/main/java/io/jenny/mynmap/MapActivity.java
+++ MyMap/app/src/main/java/io/jenny/mynmap/MapActivity.java
@@ -3,26 +3,14 @@ package io.jenny.mynmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.nhn.android.maps.NMapActivity;
-import com.nhn.android.maps.NMapView;
-import com.nhn.android.maps.nmapmodel.NMapError;
 
-public class MapActivity extends NMapActivity {
+public class MapActivity extends AppCompatActivity {
 
-    private NMapView mMapView;// 지도 화면 View
-    private final String CLIENT_ID = "jqqo8xhxhq";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mMapView = new NMapView(this);
-        setContentView(mMapView);
-        mMapView.setClientId(CLIENT_ID); // 클라이언트 아이디 값 설정
-        mMapView.setClickable(true);
-        mMapView.setEnabled(true);
-        mMapView.setFocusable(true);
-        mMapView.setFocusableInTouchMode(true);
-        mMapView.requestFocus();
+
 
     }
 }
